1db712e944108f8ea05c7779dcc1be28a769ed3f
Andrei Gubani <andrei.gubani22@gmail.com>
2023-06-08 12:32:39 +0200

    Made entities functional

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,15 +1,50 @@
 package entity;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 import main.GamePannel;
 import main.KeyHandler;
 
-public class Player extends Entity{
-    
+public class Player extends Entity {
+
     GamePannel gp;
     KeyHandler keyHandler;
 
     public Player(GamePannel gp, KeyHandler keyHandler) {
         this.gp = gp;
         this.keyHandler = keyHandler;
+        setDefaultValues();
+    }
+
+    public void setDefaultValues() {
+        // Set player default position
+        x = 100;
+        y = 100;
+        speed = 4;
+    }
+
+    public void update() {
+        // Upper Lift corner is 0,0. X increses to the right. Y increses downwards lmao
+        // why T_T
+
+        // Basic up-down, left-right movement
+        if (keyHandler.upPressed) {
+            y -= speed; // Same as y = y - speed
+        } else if (keyHandler.downPressed) {
+            y += speed;
+        } else if (keyHandler.leftPressed) {
+            x -= speed;
+        } else if (keyHandler.rightPressed) {
+            x += speed;
+        }
+        // TODO: Add support for diagonal movement
+        // Diagonal Up
+        // Diagonal Down
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.WHITE);
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
     }
 }
--- src/main/GamePannel.java
+++ src/main/GamePannel.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -32,7 +33,7 @@ public class GamePannel extends JPanel implements Runnable {
 
     // FPS and Frame/Update Restricting
     public final int FPS = 60;
-    private double drawInterval = 1000000000/FPS;
+    private double drawInterval = 1000000000 / FPS;
     private double delta;
     private long lastTime;
     private long currentTime;
@@ -45,9 +46,11 @@ public class GamePannel extends JPanel implements Runnable {
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true); // Try with false
         // Add our own key Listener to recognize input
-        /* Using a separate class gives us more controll over the actions than just
-           adding lambda statements for key listeners
-           Alos makes the code look better :P */
+        /*
+         * Using a separate class gives us more controll over the actions than just
+         * adding lambda statements for key listeners
+         * Alos makes the code look better :P
+         */
         this.addKeyListener(keyHandler);
         this.setFocusable(true);
     }
@@ -83,37 +86,19 @@ public class GamePannel extends JPanel implements Runnable {
                 delta--;
                 frameCount++;
             }
-            
+
             if (timer >= 1000000000) {
                 System.out.println("FPS: " + frameCount);
                 frameCount = 0;
                 timer = 0;
             }
 
-
         }
 
     }
 
     public void update() {
-        // Upper Lift corner is 0,0. X increses to the right. Y increses downwards lmao why T_T
-
-        // Basic up-down, left-right movement
-        if (keyHandler.upPressed) {
-            playerY -= playerSpeed; // Same as playerY = playerY - playerSpeed
-        }
-        else if (keyHandler.downPressed) {
-            playerY += playerSpeed;
-        }
-        else if (keyHandler.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        else if (keyHandler.rightPressed) {
-            playerX += playerSpeed;
-        }
-        // TODO: Add support for diagonal movement
-        // Diagonal Up
-        // Diagonal Down
+        player.update();
     }
 
     // Draw Method already exists in java. Use the paint component moethod of
@@ -124,9 +109,7 @@ public class GamePannel extends JPanel implements Runnable {
         // TODO: Use Graphics2D from the start instead of converting?
         // Convert Graphics g obj to Graphics2D for added functions
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.WHITE);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
