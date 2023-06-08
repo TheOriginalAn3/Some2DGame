ce62e00061eabc61123a6f9f0a999613e4599d89
Andrei Gubani <andrei.gubani22@gmail.com>
2023-06-08 16:11:32 +0200

    Changed Tile array to Map<name, Tile>

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,6 +4,10 @@ import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -11,48 +15,68 @@ import main.GamePannel;
 
 public class TileManager {
     private GamePannel gp;
-    private Tile[] tile;
+    private Map<String, Tile> tileMap = new HashMap<>(); // Map each array index to a string for better readability
 
+    private int col;
+    private int row;
+    private int x;
+    private int y;
 
     public TileManager(GamePannel gp) {
         this.gp = gp;
-        // TODO: Use List instead of array?
-        tile = new Tile[10];
         getTileImage();
     }
 
-    public void getTileImage() {
+    // Load the image for the Tile and put the Tiles in the Map
+    private void getTileImage() {
         try {
-            tile[0] = new Tile();
-            //tile[0].image = ImageIO.read(getClass().getResourceAsStream("res/tiles/grass.png")) not working, using workaround...
-            tile[0].image = ImageIO.read(new File("res/tiles/grass.png"));
+            Tile tile = new Tile();
+            tile.image = readImage("res/tiles/grass.png");
+            tileMap.put("grass", tile);
 
-            tile[1] = new Tile();
-            tile[1].image = ImageIO.read(new File("res/tiles/water.png"));
+            tile = new Tile();
+            tile.image = readImage("res/tiles/water.png");
+            tileMap.put("water", tile);
 
-            tile[2] = new Tile();
-            tile[2].image = ImageIO.read(new File("res/tiles/wall_stone.png"));
+            tile = new Tile();
+            tile.image = readImage("res/tiles/wall_stone.png");
+            tileMap.put("stone_wall", tile);
+
+
+            tile.image = null;
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    private BufferedImage readImage(String pathToImg) throws IOException {
+        BufferedImage img = null;
+        File file = new File(pathToImg);
+        if (file.exists()) {
+            img = ImageIO.read(file);
+        }
+        return img;
+    }
+    public BufferedImage getTile(String tileName) {
+        return tileMap.get(tileName).image;
+    }
 
     public void draw(Graphics2D g2) {
-        int col = 0;
-        int row = 0;
-        int x = 0;
-        int y = 0;
+        col = 0;
+        row = 0;
+        x = 0;
+        y = 0;
 
         while (col < gp.maxScreenCol && row <  gp.maxScreenRow ) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            g2.drawImage(getTile("grass"), x, y, gp.tileSize, gp.tileSize, null);
             col++;
-            x += gp.tileSize;
+            x = col * gp.tileSize;
 
             if (col == gp.maxScreenCol) {
                 col = 0;
                 x = 0;
                 row++;
-                y += gp.tileSize;
+                y = row * gp.tileSize;
             }
         }
     }
